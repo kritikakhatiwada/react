61446ed544a883dd2ab3a0982533f2c1a7ef1c94
Niraj Paudel <77-bca.12@texasintl.edu.np>
2023-09-11 23:39:37 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ export default function App() {
       <About />
   <Contact />
        <Countries /> */}
-      <Drinks />
       <Header />
+      {/* <Drinks /> */}
       <Routing />
     </div>
   );
--- src/components/Drinks.jsx
+++ src/components/Drinks.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { getRandomDrinks } from "../apis/drink";
+import "./Drinks.css"; 
 
 export default function Drinks() {
-  const [drinks, setDrinks] = React.useState([]);
+  const [drinks, setDrinks] = React.useState({});
+
   React.useEffect(() => {
     getRandomDrinks().then((res) => {
       setDrinks(res?.drinks?.[0]);
     });
   }, []);
-  return <div>{drinks?.strDrink}</div>;
+
+  return (
+    <div className="drink-card">
+      {drinks?.strDrinkThumb && (
+        <img
+          src={drinks.strDrinkThumb}
+          alt={drinks.strDrink}
+          className="drink-image"
+        />
+      )}
+      <h1>{drinks?.strDrink}</h1>
+    </div>
+  );
 }
